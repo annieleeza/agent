af53eaadf8fa3620928052c71e326372baf7f12d
annieleeza <annieleeza2@gmail.com>
2026-09-08 09:58:46 +0530

    Fix imports and set up Flask application
    Fixes import statements and defines Flask app with routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
+import os , utllib.parse , urillb.request , render_templates
+form app.youtube import youtube_bp
 
+Gemini_api_key = "Gemini_API_Key";
+
+def home():
+  return render template("index.html")
+  
+def create_app():
+  app=Flask(_name_)
+  app.register_blurprint(youtube_bp, url_prefix="/youtube")
+  
+@app.route("/html")
+def html():
+  return render_template("index.html")
+  
+return app;
