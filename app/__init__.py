79373f83969770db30f4ad8bef9106491cd306f9
annieleeza <annieleeza2@gmail.com>
2026-09-10 15:09:01 +0530

    Implement Flask app structure with routes
    Added routes for home, html, and health endpoints along with CORS support.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,35 @@
 import os
 from flask import Flask,request,jsonify,render_template
 from flask_cors import CORS 
+
+from app.gmail import(
+  is_email_command,
+  extract_email,
+  create_email_with_gemini
+)
+
+from app.youtube import youtube_bp
+
+def create_app():
+  app=Flask(__name__)
+  CORS(app)
+
+app.register_blueprint(
+  youtube_bp,
+  url_prefix="/youtube"
+)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/html")
+def html():
+  return render_template("index.html")
+
+@app.route("/health")
+def health 
+  
+
+
+
