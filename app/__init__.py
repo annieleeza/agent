0e7bb438c88a5d351a48883312ecafb42833bca6
annieleeza <annieleeza2@gmail.com>
2026-09-10 14:55:12 +0530

    Update app initialization with CORS support
    Refactored create_app function to include CORS and additional imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,3 @@
-from flask import Flask, render_template
-from app.youtube import youtube_bp
-
-
-def create_app():
-
-    app = Flask(__name__)
-
-    app.register_blueprint(
-        youtube_bp,
-        url_prefix="/youtube"
-    )
-
-    @app.route("/")
-    def home():
-        return render_template("index.html")
-
-    @app.route("/html")
-    def html():
-        return render_template("index.html")
-
-    return app
+import os
+from flask import Flask,request,jsonify,render_template
+from flask_cors import CORS 
